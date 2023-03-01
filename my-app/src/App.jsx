c1da6e55f8affc40008190161856c0fc9f351ad0
mamenDseid <mamelala@gmail.com>
2023-03-01 16:23:08 +0000

    vcghj

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
+import { Button } from "@suid/material";
+import { Routes, Route } from "@solidjs/router"
 
+import Home from "./pages/Home"
+import Users from "./pages/Users"
 function App() {
   return (
     <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+        
+        <h1>My Site with Lots of Pages</h1>
+      <Routes>
+        <Route path="/users" component={Users} />
+        <Route path="/" component={Home} />
+        <Route path="/about" element={<div>This site was made with Solid</div>} />
+      </Routes>
     </div>
   );
 }
