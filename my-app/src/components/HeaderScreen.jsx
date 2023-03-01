a414279f323167f7d5cc392d8e600fed7038031e
mamenDseid <mamelala@gmail.com>
2023-03-01 17:26:18 +0000

    vcgh

--- my-app/src/components/HeaderScreen.jsx
+++ my-app/src/components/HeaderScreen.jsx
@@ -1,9 +1,25 @@
-
-
+import styles from './HeaderScreen.module.css'
+import { IconButton } from "@suid/material";
+import {
+  FavoriteBorderOutlined,
+  Menu,
+  MailOutlined,
+  SearchOutlined
+} from "@suid/icons-material";
 export default function HeaderScreen() {
   return (
-    <header class='mt'>
-<h1>header </h1>
+    <header class={styles.mt}>
+<div class="">
+        <IconButton>
+          <Menu />
+        </IconButton>
+      </div>
+      <div class="title">SHEIN</div>
+      <div class="">
+        <IconButton>
+          <SearchOutlined sx={{}} />
+        </IconButton>
+      </div>
     </header>
   )
 }
