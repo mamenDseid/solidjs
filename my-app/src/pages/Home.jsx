e2d88bd9dad1cb012d81a3e176eb3c6d61c5a6e9
mamenDseid <mamelala@gmail.com>
2023-03-03 14:46:19 +0000

    cvsj

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,10 +1,23 @@
-import HeaderScreen from "../components/HeaderScreen";
+import { repos, setUserName, username } from '../App'
 
-
-export default function Home() {
+function Home() {
+  const refetchWithUsername = (e) => {
+    e.preventDefault()
+    const usernameInput = document.querySelector('#usernameInput')
+    setUserName(usernameInput.value)
+  }
   return (
     <div>
-      <HeaderScreen/>
+      <form class='class="mb-3'
+        onSubmit={(e) => refetchWithUsername(e)}
+      >
+        <input type="text" class="p-1 align-middle" placeholder="Type username..." id="usernameInput" required />
+
+        <button class="btn btn-dark ms-3 w-auto" type="submit">
+          Fetch
+        </button>
+      </form>
     </div>
-  )
+  );
 }
+export default Home;
