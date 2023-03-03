c62c44a4679edff031725ca9082edb0bf86b48b4
mamenDseid <mamelala@gmail.com>
2023-03-03 14:19:14 +0000

    cdfsy

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,21 +1,10 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
-import { Button } from "@suid/material";
-import { Routes, Route } from "@solidjs/router"
 
-import Home from "./pages/Home"
-import Users from "./pages/Users"
-function App() {
+ function App() {
+
   return (
-    <div class={styles.App}>
-        
-      <Routes>
-        <Route path="/users" component={Users} />
-        <Route path="/" component={Home} />
-        <Route path="/about" element={<div>This site was made with Solid</div>} />
-      </Routes>
+    <div>
+      <h4>Hello world</h4>
     </div>
   );
 }
-
-export default App;
+  export default App;
