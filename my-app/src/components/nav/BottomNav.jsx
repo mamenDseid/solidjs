aad82a562e3f0bc8217fd2a451973dfa992069fe
mamenDseid <mamelala@gmail.com>
2023-04-18 00:29:42 +0000

    cd

--- my-app/src/components/nav/BottomNav.jsx
+++ my-app/src/components/nav/BottomNav.jsx
@@ -4,6 +4,7 @@ import  ManageSearchOutlinedIcon from "@suid/icons-material/ManageSearchOutlined
 import  PersonOutlineOutlinedIcon from "@suid/icons-material/PersonOutlineOutlined"
 import HomeIcon from "@suid/icons-material/Home";
 import ShoppingBagOutlinedIcon from "@suid/icons-material/ShoppingBagOutlined"
+import DressOutlined from "./DressOutlined";
 function BottomNav() {
 
     return (
@@ -28,19 +29,24 @@ function BottomNav() {
                         </li>
                         <li>
                             <span class="navspan">
+                                <DressOutlined/>
                                 <span>New</span>
 
                             </span>
                         </li>
                         <li>
                             <span class="navspan">
+                                <ShoppingBagOutlinedIcon/>
                                 <span>
-                                    Me
+                                    Bag
                                 </span>
                             </span>
                         </li>
                         <li>
-                            <span>five</span>
+                            <span class="navspan">
+                                <PersonOutlineOutlinedIcon/>
+                                <span>Me</span>
+                            </span>
                         </li>
                     </ul>
                 </nav>
